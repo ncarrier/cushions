df4b0bc18c733e60e633378a75d59ab9dd485b0d
Nicolas Carrier <carrier.nicolas0@gmail.com>
2016-12-19 07:51:24 +0100

    dev: added the stringify macro implementation

--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,10 @@
 #define _UTILS_H
 #include <stdbool.h>
 
+/* only used for STRINGIFY implementation */
+#define STRINGIFY_HELPER(s) #s
+#define STRINGIFY(s) STRINGIFY_HELPER(s)
+
 bool string_matches_prefix(const char *string, const char *prefix);
 void string_cleanup(char **s);
 
