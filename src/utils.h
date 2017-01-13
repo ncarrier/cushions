09b8b13f918343fe2fc2af88e9ca84f02f3af399
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-13 08:37:55 +0100

    dev: added container_of definition

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,18 @@
 #ifndef _UTILS_H
 #define _UTILS_H
+#include <stdlib.h>
+#include <stddef.h>
 #include <stdbool.h>
 
 /* only used for STRINGIFY implementation */
 #define STRINGIFY_HELPER(s) #s
 #define STRINGIFY(s) STRINGIFY_HELPER(s)
+
+#ifndef container_of
+#define container_of(ptr, type, member) ({ \
+	const typeof(((type *)NULL)->member)*__mptr = (ptr); \
+	(type *)((uintptr_t)__mptr - offsetof(type, member)); })
+#endif /* container_of */
 
 bool string_matches_prefix(const char *string, const char *prefix);
 void string_cleanup(char **s);
