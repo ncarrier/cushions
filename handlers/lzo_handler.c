1fc589480e07580a27223eba9175f2d7e23a4ac8
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-19 22:36:33 +0100

    dev: make lzo use a fixed block size buffer

--- handlers/lzo_handler.c
+++ handlers/lzo_handler.c
@@ -1,4 +1,6 @@
 #define _GNU_SOURCE
+#include <sys/param.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
@@ -17,10 +19,12 @@
 #define LOG_TAG lzo_handler
 #include "log.h"
 
-#define BUFFER_SIZE 0x400
 /* hardcode the mode, we don't have information on the original file */
 #define DEFAULT_MODE 0x81ed
 #define PLACEHOLDER "placeholder"
+
+#define B_SIZE 0x40000
+#define MAX_COMPRESSED_SIZE(x)  ((x) + (x) / 16 + 64 + 3)
 
 #define LZOP_MAGIC_INITIALIZER { 0x89, 0x4c, 0x5a, 0x4f, 0x00, 0x0d, 0x0a, \
 	0x1a, 0x0a }
@@ -42,10 +46,13 @@ struct lzo_cushions_handler {
 struct lzo_cushions_file {
 	int error;
 	FILE *file;
-	char buffer[LZO1X_1_MEM_COMPRESS];
-	lzo_bytep dest;
-	size_t dest_size;
-	bool eof;
+	char wrkmem[LZO1X_1_MEM_COMPRESS];
+	/* where original data will be copied */
+	uint8_t src[B_SIZE];
+	/* next byte to write in src */
+	size_t cur;
+	/* where compressed data are stored before being written to file */
+	uint8_t dst[MAX_COMPRESSED_SIZE(B_SIZE)];
 	/* for a starter, we will use only crc32 checksum */
 	uint32_t checksum;
 };
@@ -78,19 +85,74 @@ struct lzop_block_header {
 
 static struct lzo_cushions_handler lzo_cushions_handler;
 
-static int lzo_close(void *c)
+static int write_compressed_block(struct lzo_cushions_file *file)
 {
-	struct lzo_cushions_file *lzo_c_file = c;
-	uint32_t size = 0;
+	ssize_t sret;
+	lzo_uint dst_len;
+	uint32_t final_size;
+	uint8_t *final_buf;
+	struct lzop_block_header header;
 
-	/* lzop ends the file with a 0 size */
-	fwrite(&size, sizeof(size), 1, lzo_c_file->file);
-	if (lzo_c_file->file != NULL)
-		fclose(lzo_c_file->file);
-	memset(lzo_c_file, 0, sizeof(*lzo_c_file));
-	free(lzo_c_file);
+	/* can't fail */
+	lzo1x_1_15_compress((const lzo_bytep)file->src, file->cur, file->dst,
+			&dst_len,file->wrkmem);
+
+	final_size = dst_len < file->cur ? dst_len : file->cur;
+	header = (struct lzop_block_header) {
+		.block_size = htonl(file->cur),
+		.compressed_block_size = htonl(final_size),
+		.block_checksum = htonl(lzo_adler32(1, file->src, file->cur)),
+	};
+
+	sret = fwrite(&header, sizeof(header), 1, file->file);
+	if (sret != 1) {
+		sret = -errno;
+		LOGPE("fwrite header", sret);
+		return sret;
+	}
+	final_buf = dst_len < file->cur ? file->dst : file->src;
+	sret = fwrite(final_buf, final_size, 1, file->file);
+	if (sret != 1) {
+		sret = -errno;
+		LOGPE("fwrite block", errno);
+		return sret;
+	}
+	file->cur = 0;
 
 	return 0;
+}
+
+static int lzo_close(void *c)
+{
+	int ret;
+	int ret_val = 0;
+	struct lzo_cushions_file *file = c;
+	uint32_t size = 0;
+	int old_errno = 0;
+
+	/* write remaining data if needed */
+	if (file->cur != 0) {
+		ret = write_compressed_block(file);
+		if (ret < 0) {
+			LOGPE("write_compressed_block", ret);
+			old_errno = -ret;
+			ret_val = EOF;
+		}
+	}
+	/* lzop ends the file with a 0 size */
+	ret = fwrite(&size, sizeof(size), 1, file->file);
+	if (ret < 0) {
+		LOGPE("fwrite last 0 byte", ret);
+		old_errno = -ret;
+		ret_val = EOF;
+	}
+	if (file->file != NULL)
+		fclose(file->file);
+	memset(file, 0, sizeof(*file));
+	free(file);
+	errno = old_errno;
+
+	return ret_val;
 }
 
 static void compute_checksum(struct lzop_file_header *h)
@@ -189,19 +251,20 @@ err:
 	return NULL;
 }
 
-static int grow_dest_buffer(struct lzo_cushions_file *file, size_t new_size)
+static int store(struct lzo_cushions_file *f, const char *buf, size_t size)
 {
-	void *tmp;
-	if (new_size < file->dest_size)
-		return 0;
+	int written;
+	int remaining;
 
-	tmp = realloc(file->dest, new_size);
-	if (tmp == NULL)
-		return -errno;
-	file->dest = tmp;
-	file->dest_size = new_size;
+	if (f->cur >= B_SIZE)
+		return -ENOMEM;
 
-	return 0;
+	remaining = B_SIZE - f->cur;
+	written = MIN(size, remaining);
+	memcpy(f->src + f->cur, buf, written);
+	f->cur += written;
+
+	return written;
 }
 
 /**
@@ -223,57 +286,28 @@ static int grow_dest_buffer(struct lzo_cushions_file *file, size_t new_size)
  */
 static ssize_t lzo_write(void *cookie, const char *buf, size_t size)
 {
-	/*
-	 * TODO add an intermediate buffering step to avoid writing many small
-	 * blocks
-	 */
 	int ret;
-	size_t uncompressible_data_size;
-	struct lzo_cushions_file *f = cookie;
-	lzo_uint dst_len;
-	size_t sret;
-	struct lzop_block_header header;
+	size_t consumed = 0;
+	struct lzo_cushions_file *file = cookie;
 
-	/*
-	 * from doc/LZO.txt:
-	 * When dealing with incompressible data, LZO expands the input block by
-	 * a maximum of 64 bytes per 1024 bytes input.
-	 */
-	uncompressible_data_size = (((size >> 10) + 1) << 6) + size;
-	ret = grow_dest_buffer(f, uncompressible_data_size);
-	if (ret < 0) {
-		LOGPE("grow_dest_buffer", ret);
-		errno = -ret;
-		return 0;
-	}
-	/* can't fail */
-	lzo1x_1_15_compress((const lzo_bytep)buf, size, f->dest, &dst_len,
-			f->buffer);
-
-	header = (struct lzop_block_header) {
-		.block_size = htonl(size),
-		.compressed_block_size = htonl(dst_len < size ? dst_len : size),
-		.block_checksum = htonl(lzo_adler32(1, (uint8_t *)buf, size)),
-	};
-
-	sret = fwrite(&header, sizeof(header), 1, f->file);
-	if (sret != 1) {
-		LOGPE("fwrite header", errno);
-		return 0;
-	}
-	if (dst_len < size) {
-		sret = fwrite(f->dest, dst_len, 1, f->file);
-		if (sret != 1) {
-			LOGPE("fwrite block", errno);
+	do {
+		ret = store(file, buf, size);
+		if (ret < 0) {
+			LOGPE("store", ret);
+			errno = -ret;
 			return 0;
 		}
-	} else {
-		sret = fwrite(buf, size, 1, f->file);
-		if (sret != 1) {
-			LOGPE("fwrite block", errno);
-			return 0;
+		if (file->cur < B_SIZE)
+			return size;
+		consumed += ret;
+
+		ret = write_compressed_block(file);
+		if (ret < 0) {
+			LOGPE("write_compressed_block", ret);
+			errno = -ret;
+			return EOF;
 		}
-	}
+	} while (consumed != size);
 
 	return size;
 }
