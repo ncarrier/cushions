da534a71e503ff5e9a1ad759c04222a5839abe4f
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-19 07:53:06 +0100

    dev: implementation of mem_handler

--- handlers/mem_handler.c
+++ handlers/mem_handler.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <errno.h>
+#include <inttypes.h>
 
 #include <cushions.h>
 #include <cushions_handler.h>
@@ -13,34 +14,129 @@ struct mem_cushions_handler {
 	cookie_io_functions_t mem_func;
 };
 
+#define to_mem_handler(h) container_of((h), struct mem_cushions_handler, handler)
+
 struct mem_cushions_file {
-	FILE *file;
+	FILE *dest_file;
+	FILE *mem_file;
 	char *buffer;
 	size_t size;
 };
+
+static int mem_close(void *cookie)
+{
+	int old_errno = 0;
+	int ret;
+	ssize_t sret;
+	struct mem_cushions_file *mf = cookie;
+
+	if (mf == NULL) {
+		errno = EINVAL;
+		return EOF;
+	}
+
+	if (mf->mem_file != NULL) {
+		ret = fclose(mf->mem_file);
+		if (ret == EOF) {
+			old_errno = errno;
+			LOGPE("fclose", old_errno);
+			mf->mem_file = NULL;
+			mem_close(cookie);
+			errno = old_errno;
+			return EOF;
+		}
+	}
+	if (mf->dest_file == NULL) {
+		errno = EINVAL;
+		return EOF;
+	} else {
+		if (mf->buffer != NULL) {
+			LOGI("write all data at once");
+			sret = fwrite(mf->buffer, mf->size, 1, mf->dest_file);
+			if (sret < 1) {
+				old_errno = errno;
+				LOGPE("fwrite", ret);
+			}
+		}
+		ret = fclose(mf->dest_file);
+		if (ret == EOF) {
+			if (old_errno == 0)
+				old_errno = errno;
+			LOGPE("fclose", old_errno);
+		}
+	}
+	if (mf->buffer != NULL)
+		free(mf->buffer);
+
+	free(cookie);
+
+	if (old_errno != 0) {
+		errno = old_errno;
+		return EOF;
+	}
+
+	return 0;
+}
 
 static FILE *mem_cushions_fopen(struct cushions_handler *handler,
 		const char *path, const char *full_path, const char *scheme,
 		const struct cushions_handler_mode *mode)
 {
-	errno = -ENOSYS;
+	int old_errno;
+	struct mem_cushions_file *mf;
+	struct mem_cushions_handler *h = to_mem_handler(handler);
 
-//	FILE *open_memstream(char **ptr, size_t *sizeloc);
+	/* TODO less restrictive condition */
+	if (strcmp(mode->mode, "w") != 0 && strcmp(mode->mode, "wb") != 0) {
+		LOGE("lzo scheme only supports \"r\" open mode");
+		errno = EINVAL;
+		return NULL;
+	}
+
+	mf = calloc(1, sizeof(*mf));
+	if (mf == NULL) {
+		old_errno = errno;
+		LOGPE("calloc", errno);
+		errno = old_errno;
+		return NULL;
+	}
+	mf->dest_file = cushions_fopen(path, mode->mode);
+	if (mf->dest_file == NULL) {
+		old_errno = errno;
+		LOGPE("cushions_fopen", errno);
+		goto err;
+	}
+
+	mf->mem_file = open_memstream(&mf->buffer, &mf->size);
+	if (mf->dest_file == NULL) {
+		old_errno = errno;
+		LOGPE("open_memstream", errno);
+		goto err;
+	}
+
+	/* TODO adapt mode according to the mode argument */
+	return fopencookie(mf, mode->mode, h->mem_func);
+err:
+
+	mem_close(mf);
+
+	errno = old_errno;
+
 	return NULL;
-}
-
-static int mem_close(void *c)
-{
-	errno = -ENOSYS;
-
-	return 0;
 }
 
 static ssize_t mem_write(void *cookie, const char *buf, size_t size)
 {
-	errno = -ENOSYS;
+	ssize_t sret;
+	struct mem_cushions_file *mf = cookie;
 
-	return 0;
+	sret = fwrite(buf, size, 1, mf->mem_file);
+	if (sret < 0) {
+		LOGPE("fwrite", errno);
+		return 0;
+	}
+
+	return size;
 }
 
 static struct mem_cushions_handler mem_cushions_handler = {
