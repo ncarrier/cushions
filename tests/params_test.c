5ec2de3f6bf4f0ec18025ab43f26c87432cc10df
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-20 18:11:00 +0100

    devminor: use logging system in params_test

--- tests/params_test.c
+++ tests/params_test.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <envz.h>
 
+#define LOG_TAG params_test
 #include <cushions_handler.h>
 
 #define assert_str_equal(s1, s2) assert(strcmp((s1), (s2)) == 0)
@@ -16,7 +17,7 @@ int main(void)
 	size_t len;
 
 	input = "/tata/tutu/toto?plop=wizz;foo=bar;baz=";
-	fprintf(stderr, "input is \"%s\"\n", input);
+	LOGI("input is \"%s\"", input);
 	assert(cushions_handler_break_params(input, &path, &envz, &len) == 0);
 	assert_str_equal(path, "/tata/tutu/toto");
 	assert_str_equal(envz_get(envz, len, "plop"), "wizz");
@@ -27,7 +28,7 @@ int main(void)
 	free(envz);
 
 	input = "/tata/tutu/toto?";
-	fprintf(stderr, "input is \"%s\"\n", input);
+	LOGI("input is \"%s\"", input);
 	assert(cushions_handler_break_params(input, &path, &envz, &len) == 0);
 	assert_str_equal(path, "/tata/tutu/toto");
 	assert_null(envz);
@@ -35,14 +36,14 @@ int main(void)
 	free(path);
 
 	input = "";
-	fprintf(stderr, "input is \"%s\"\n", input);
+	LOGI("input is \"%s\"", input);
 	assert(cushions_handler_break_params(input, &path, &envz, &len) == 0);
 	assert_null(envz);
 	assert_null(path);
 	assert(len == 0);
 
 	input = "?plop=wizz;foo=bar;baz";
-	fprintf(stderr, "input is \"%s\"\n", input);
+	LOGI("input is \"%s\"", input);
 	assert(cushions_handler_break_params(input, &path, &envz, &len) == 0);
 	assert_str_equal(path, "");
 	assert_str_equal(envz_get(envz, len, "plop"), "wizz");
