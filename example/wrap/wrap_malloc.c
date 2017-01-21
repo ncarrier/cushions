30fc83d8a0611bcbb0df2a7b9276c0d49934a1bb
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-21 08:14:54 +0100

    warningfix: __wrap_malloc missing prototype

--- example/wrap/wrap_malloc.c
+++ example/wrap/wrap_malloc.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "wrap_malloc.h"
+
 void * __real_malloc (size_t c);
 
 void * __wrap_malloc (size_t c)
 {
 	printf ("malloc called with %zu\n", c);
-	return __real_malloc (c);
+	return __real_malloc(c);
 }
-
-
---
+++ example/wrap/wrap_malloc.h
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_WRAP_WRAP_MALLOC_H_
+#define EXAMPLE_WRAP_WRAP_MALLOC_H_
+
+void * __wrap_malloc (size_t c);
+
+#endif /* EXAMPLE_WRAP_WRAP_MALLOC_H_ */
