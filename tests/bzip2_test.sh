f15aedc9beae002852223c25d3a3a13912f856bd
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-23 07:58:22 +0100

    dev: compression test for the bzip2 handler

--- tests/bzip2_test.sh
+++ tests/bzip2_test.sh
@@ -12,10 +12,21 @@ set -xeu
 trap on_exit EXIT
 
 mkdir -p ${wdir}
+
 cd ${wdir}
 dd if=/dev/urandom of=tutu bs=1024 count=1024
+
+# decompression
 bzip2 -fk tutu
 cd ${cwd}; ./cpw bzip2://${wdir}/tutu.bz2 ${wdir}/toto; cd -
 md5sum tutu > tutu.md5
 md5sum toto > toto.md5
 diff tutu toto
+
+# compression
+cd ${cwd}; ./cpw ${wdir}/tutu bzip2://${wdir}/tutu.bz2; cd -
+bzip2 --decompress ${wdir}/tutu.bz2 --stdout > toto
+md5sum tutu > tutu.md5
+md5sum toto > toto.md5
+diff tutu toto
+
