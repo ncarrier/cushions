35b9f767a146dffd2dc7850483b1dd34ca653746
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-25 06:25:13 +0100

    dev: log the loading of the file handler

--- src/file_handler.c
+++ src/file_handler.c
@@ -26,6 +26,8 @@ static __attribute__((constructor)) void file_cushions_handler_constructor(void)
 {
 	int ret;
 
+	LOGI(__func__);
+
 	ret = cushions_handler_register(&file_cushions_handler.handler);
 	if (ret < 0)
 		LOGW("cushions_handler_register(file_cushions_handler) "
