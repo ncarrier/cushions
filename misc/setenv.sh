492c9c750bd81d2f3731e00e0ef9fd8682a1216e
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-27 07:35:26 +0100

    dev: use absolute paths in setenv.sh

--- misc/setenv.sh
+++ misc/setenv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LD_LIBRARY_PATH=. CUSHIONS_HANDLERS_PATH=handlers_dir CUSHIONS_LOG_LEVEL=3 "$@"
+LD_LIBRARY_PATH=$(realpath .) CUSHIONS_HANDLERS_PATH=$(realpath handlers_dir) CUSHIONS_LOG_LEVEL=3 "$@"
