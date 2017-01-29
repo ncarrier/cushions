fd5e41df410ffd71c8f3fadbe8716534fafbc5c7
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-01-29 10:01:19 +0100

    dev: track unitialized variables accesses in valgrind

--- misc/valgrind.sh
+++ misc/valgrind.sh
@@ -2,6 +2,7 @@
 
 
 ../misc/setenv.sh valgrind --leak-check=full \
+		--track-origins=yes \
 		--show-leak-kinds=all \
 		--suppressions=../misc/valgrind.supp \
 		--suppressions=../misc/curl_ssh2_valgrind.supp \
