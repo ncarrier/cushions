a9864cf1cfdd468cd68a6e2d474cfe90f476546e
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-02-18 07:42:55 +0100

    doc: doxygen for cushions.h

--- include/cushions/cushions.h
+++ include/cushions/cushions.h
@@ -4,7 +4,30 @@
 
 #include <cushions_common.h>
 
+/**
+ * This function opens the file whose name is the string pointed to by *path*
+ * and associates a stream with it.
+ * @param path Name of the file to open, if it starts with SCHEME://, with
+ * SCHEME being an URL scheme for which a scheme handler is registered, then
+ * this handler will take care of the opening, otherwise, fopen will be used.
+ * Multiple schemes can be chained provided the handler for the outermost
+ * schemes do rely themselves on cushions_fopen.
+ * @param mode Open mode string, such as "rbe", as described in fopen(3)
+ * @return Returns a FILE pointer upon successful completion. Otherwise, NULL is
+ * returned and errno is set to indicate the error.
+ * @see fopen(3)
+ */
 CUSHIONS_API FILE *cushions_fopen(const char *path, const char *mode);
+
+/**
+ * Alias to cushions_fopen to use with linker option -Wl,--wrap=fopen.
+ * @param path Path to open.
+ * @param mode Open string mode.
+ * @return Returns a FILE pointer upon successful completion. Otherwise, NULL is
+ * returned and errno is set to indicate the error.
+ * @see fopen(3)
+ * @see cushions_fopen
+ */
 CUSHIONS_API FILE *__wrap_fopen(const char *path, const char *mode);
 
 #endif /* _CUSHIONS_H */
