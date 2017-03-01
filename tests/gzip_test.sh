c605fe9eb3d6d0c74332548d8931864d9c96463f
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-03-01 17:56:08 +0100

    dev: tests for gzip decompression

--- tests/gzip_test.sh
+++ tests/gzip_test.sh
@@ -15,6 +15,12 @@ trap on_exit EXIT
 
 dd if=/dev/urandom of=${wdir}tutu bs=1024 count=1024
 
+# decompression
+gzip -fck ${wdir}tutu > ${wdir}tutu.gz
+${cp} gzip://${wdir}tutu.gz ${wdir}toto
+cmp ${wdir}tutu ${wdir}toto
+rm ${wdir}tutu.gz
+
 # compression
 ${cp} ${wdir}tutu gzip://${wdir}tutu.gz
 gunzip ${wdir}tutu.gz --stdout > ${wdir}toto
