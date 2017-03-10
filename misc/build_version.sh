7bc6d03f33eaa51c00af525a9fe3ecc07f789d14
Nicolas Carrier <carrier.nicolas0@gmail.com>
2017-03-10 07:21:03 +0100

    build: build the docker images before building the packages

--- misc/build_version.sh
+++ misc/build_version.sh
@@ -8,6 +8,7 @@ fi
 set -xeu
 
 version=$1
+start_dir=$PWD
 
 rm -rf build_version
 
@@ -18,7 +19,10 @@ make -f ../Makefile -j $(nproc) CU_NEW_VERSION=${version} version
 
 docker_cmd="docker run --rm -v ${OLDPWD}:/workspace"
 
-for d in trusty xenial precise; do
-	${docker_cmd} ${d}_for_cushions ./misc/make_archive.sh &
+cd ${start_dir}
+for f in $(find docker/ -name Dockerfile); do
+	(d=$(echo $f | cut -d / -f 3)
+	docker build -t ${d}_for_cushions $(dirname ${f})
+	${docker_cmd} ${d}_for_cushions ./misc/make_archive.sh) &
 done
 wait
